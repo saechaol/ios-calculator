eb475dee2bbb1b6fb236f1339c4f2750bcc7b202
Lucas <33850575+saechaol@users.noreply.github.com>
2020-10-28 18:43:11 -0700

    Implemented PaddedLabel subclass

---
+++ Calculator/Labels/PaddedLabel.swift
@@ -0,0 +1,26 @@
+//
+//  PaddedLabel.swift
+//  Calculator
+//
+//  Created by Lucas Saechao on 10/28/20.
+//
+
+import UIKit
+
+@IBDesignable class PaddedLabel: UILabel {
+    
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
+    @IBInspectable var leftInset: CGFloat = 0.0
+    @IBInspectable var rightInset: CGFloat = 0.0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + (leftInset + rightInset), height: size.height + (topInset + bottomInset))
+    }
+}
